636d2f1f2d6310c82bb6790b640664d59dbe632c
Rayan Batada <rayanbatada@gmail.com>
2025-10-27 14:32:25 -0400

    updated page.tsx and post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -12,8 +12,6 @@ import FlagButton from "~/components/FlagButton";
 import VoteButton from "~/components/VoteButton";
 import ShareDropdown from "~/components/ShareDropdown";
 import formatEventTime from "~/lib/formatEventTime";
-// import { getSessionUser } from "~/server/auth";
-// import { db } from "~/server/db";
 import type {
   events,
   postVotes,
@@ -41,27 +39,25 @@ export default function Post({
       className="rounded-md border border-gray-300 bg-white px-2"
     >
       <div className="flex flex-col gap-2 px-2 py-4">
-        <div className="flex items-start gap-3">
-          <Link
-            href={`/users/${author.id}`}
-            className="group flex flex-1 items-center gap-3 text-3xl"
-          >
-            <Avatar {...author} />
-            <span className="flex flex-col gap-0.5">
-              <span className="text-sm leading-none font-bold group-hover:underline">
-                {author.name}
+        <div className="flex items-start justify-between">
+          <div className="flex items-start gap-3">
+            <Link
+              href={`/users/${author.id}`}
+              className="group flex items-center gap-3 text-3xl"
+            >
+              <Avatar {...author} />
+              <span className="flex flex-col gap-0.5">
+                <span className="text-sm leading-none font-bold group-hover:underline">
+                  {author.name}
+                </span>
+                <span className="text-xs leading-none text-gray-600 capitalize">
+                  {author.type}
+                </span>
               </span>
-              <span className="text-xs leading-none text-gray-600 capitalize">
-                {author.type}
-              </span>
-            </span>
-          </Link>
+            </Link>
+          </div>
 
-          {session && <FlagButton postId={post.id} userId={session?.userId} />}
-
-          <button className="-m-0.5 rounded-full p-0.5 hover:bg-gray-200">
-            <PiDotsThreeBold />
-          </button>
+          {session && <FlagButton postId={post.id} userId={session.userId} />}
         </div>
 
         {post.content && (
@@ -97,29 +93,48 @@ export default function Post({
             </button>
           </Link>
         )}
-      </div>
 
-      <div className="flex justify-between gap-2 border-t border-t-gray-300 px-2 py-3 text-gray-700">
-        <div className="flex flex-wrap items-center justify-start gap-1 border-t border-gray-200 px-3 py-2 text-xs">
-          <p className="ml-auto px-2 text-gray-500">
+        <div className="flex items-center justify-between border-t border-t-gray-200 pt-2 text-xs">
+          <div className="flex items-center gap-2">
+            <Link
+              href="#"
+              className="flex items-center gap-1 text-sky-900/70 hover:text-sky-900 hover:underline"
+            >
+              <PiHash />
+              Dev Dogs
+            </Link>
+          </div>
+          <p className="text-gray-500">
             {formatDistanceToNowStrict(post.createdAt)} ago
           </p>
         </div>
-      </div>
 
-      <div className="flex justify-between gap-2 border-t border-t-gray-300 px-2 py-3 text-gray-700">
-        <VoteButton
-          target={{ postId: post.id }}
-          score={post.score ?? 0}
-          value={vote}
-        />
-        <button className="flex items-center gap-2 rounded-sm px-2 py-1 leading-none hover:bg-green-100">
-          <PiChatCircleTextBold />
-          {/* <span className="text-xs font-semibold">33</span> */}
-        </button>
-        <ShareDropdown
-          link={`https://community-resource-forum.vercel.app/events/${post.eventId}`}
-        />
+        <div className="flex items-center gap-2 border-t border-t-gray-300 py-3 pr-2 pl-1 text-gray-700">
+          <Link
+            className="flex items-center gap-1.5 rounded-full px-2 py-1 leading-none hover:bg-sky-100 hover:ring hover:ring-sky-800"
+            href={`/discussion/${post.id}?comment`}
+          >
+            <PiChatCircleTextBold />
+            <span className="text-xs font-semibold">0</span>
+          </Link>
+
+          <ShareDropdown
+            permalink={`https://community-resource-forum.vercel.app/discussion/${post.id}`}
+          >
+            <button className="flex items-center gap-1.5 rounded-full px-2 py-1 leading-none hover:bg-sky-100 hover:ring hover:ring-sky-800">
+              <PiShareFatBold />
+              <span className="text-xs font-semibold">Share</span>
+            </button>
+          </ShareDropdown>
+
+          <div className="ml-auto scale-90">
+            <VoteButton
+              target={{ postId: post.id }}
+              score={post.score ?? 0}
+              value={vote}
+            />
+          </div>
+        </div>
       </div>
     </article>
   );
